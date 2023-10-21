9ea6b5d9a8e2ebd23520f8db2eba3ae902895ff5
Clayton Augusto Nascimento de Araújo <augustoclayton57@gmail.com>
2023-10-21 13:43:35 -0300

    arrumando

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CafeDoGloriaCadastro</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <div class="menu-bar">
+        <ul>
+            <li class="li">Espresso</li>
+            <li class="li">Latté</li>
+            <li class="li">Mocha</li>
+            <li class="li">Especiais</li>
+            <li class="li">Registrados</li>
+            <li class="li">Compras</li>
+        </ul>
+    </div>
+</body>
+</html>
