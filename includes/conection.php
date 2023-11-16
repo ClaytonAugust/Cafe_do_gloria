d56ea1a291ecbb8299dd750e02bfc17c029b8363
Natan Ayres <129100066+natan-ayres@users.noreply.github.com>
2023-11-16 10:08:23 -0300

    Update conection.php
    Troquei os nomes

--- includes/conection.php
+++ includes/conection.php
@@ -1,9 +1,10 @@
 <?php 
     $servername='localhost';//porta que o banco está
+    $database = 'cafedb';
     $username = 'root';// nume de usuário padrão
     $password='';//senha
-    $conn = new mysqli ($servername,$username,$password);
-    if (mysqli_query($conn,"CREATE DATABASE IF NOT EXISTS db_aula2")){ //criação do banco caso não exista.
+    $conn = new mysqli ($servername,$database, $username,$password);
+    if (mysqli_query($conn,"CREATE DATABASE IF NOT EXISTS cafedb")){ //criação do banco caso não exista.
         echo 'Banco de dados criado';
     }else{
         echo "Erro criando o banco de dados";
